bd2abbf4cd3656893bcbfb22a58a74127d1b565f
Xie Yuheng <xyheme@gmail.com>
2019-03-18 21:09:10 +0800

    up

--- src/cx.ts
+++ src/cx.ts
@@ -1,14 +1,73 @@
-export class cell_t <T> {
+// for a class of spaces
+//   we need to make sure that
+//   spaces not meant to be in class is not constructable
+
+export
+class n_ball_t {
   constructor (
-    public dimension: number,
-    public id: number,
-    public boundary: cell_complex_t <T>,
-    public value?: T,
+    public dim: number,
+    public boundary: spherical_complex_t,
   ) {}
 }
 
-export class cell_complex_t <T> {
+// a cell can only exist in the context of cell_complex
+
+export
+class n_cell_t {
+  dim: number
+
   constructor (
-    public cells: Array <cell_t <T>>,
+    public cell_complex: cell_complex_t,
+    public n_ball: n_ball_t,
+    public attaching_map: map_t,
+  ) {
+    this.dim = n_ball.dim
+  }
+}
+
+// the domain of a attaching_map
+//   is the boundary of a n_ball_t
+//   which is a spherical_complex_t
+//   which is a cell_complex_t
+// this cell_complex is not part of the cell_complex
+//   we are constructing
+
+// every map is continuous
+
+export
+class continuous_check_t {
+  // [todo]
+}
+
+export
+class map_t {
+  // [todo]
+}
+
+export
+class cell_complex_t {
+  constructor (
+    public cells: Map <{ dim: number, id: number }, n_cell_t> = new Map (),
   ) {}
 }
+
+export
+class spherical_check_t {
+  // [todo]
+}
+
+export
+class spherical_complex_t
+extends cell_complex_t {
+
+  constructor (
+    public cell_complex: cell_complex_t,
+    public spherical_check: spherical_check_t,
+  ) {
+    super (cell_complex.cells)
+  }
+}
+
+let spherical_complex = new spherical_complex_t (
+  new cell_complex_t (),
+  new spherical_check_t ())
--- tests/cx.test.js
+++ tests/cx.test.js
@@ -1,14 +1,14 @@
 import test from "ava"
 
-import { cell_complex_t, cell_t } from "../dist/cx"
+// import { cell_complex_t, cell_t } from "../dist/cx"
 
 test ("torus", t => {
-  let o = new cell_t (0, 0, new cell_complex_t ([]))
-  let p = new cell_t (1, 0, new cell_complex_t ([o, o]))
-  let q = new cell_t (1, 1, new cell_complex_t ([o, o]))
-  let m = new cell_t (2, 0, new cell_complex_t ([p, q, p, q]))
+//   let o = new cell_t (0, 0, new cell_complex_t ([]))
+//   let p = new cell_t (1, 0, new cell_complex_t ([o, o]))
+//   let q = new cell_t (1, 1, new cell_complex_t ([o, o]))
+//   let m = new cell_t (2, 0, new cell_complex_t ([p, q, p, q]))
 
-  console.log (m)
+//   console.log (m)
 
   t.pass ()
 })
