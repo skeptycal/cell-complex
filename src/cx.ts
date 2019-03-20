198a8665eafad66992cf55d7db9d2f51526bbaae
Xie Yuheng <xyheme@gmail.com>
2019-03-20 18:31:39 +0800

    up

--- src/cx.ts
+++ src/cx.ts
@@ -1,6 +1,8 @@
-// for a class of spaces
+// - for a class of spaces
 //   we need to make sure that
 //   spaces not meant to be in class is not constructable
+// - to make a class easily inheritable
+//   its constructor should not take arguments
 
 export
 class id_t {
@@ -43,8 +45,8 @@ class cell_t
 extends cmap_t {
   constructor (
     public dim: number,
-    public cell_complex: cell_complex_t,
     public boundary: spherical_complex_t,
+    public cell_complex: cell_complex_t,
     public attaching_map: Map <id_t, id_t>,
   ) {
     super (boundary, cell_complex, attaching_map)
@@ -60,63 +62,182 @@ extends cmap_t {
 
 export
 class cell_complex_t {
-  point_set: Set <id_t>;
+  point_array: Array <id_t>;
   cell_map: Map <id_t, cell_t>;
 
   constructor () {
-    this.point_set = new Set ()
+    this.point_array = new Array ()
     this.cell_map = new Map ()
   }
 
   clone (): cell_complex_t {
     let cell_complex = new cell_complex_t ()
-    cell_complex.point_set = new Set (this.point_set)
+    cell_complex.point_array = this.point_array.slice ()
     cell_complex.cell_map = new Map (this.cell_map)
     return cell_complex
   }
 
-  inc_points (n: number): Array <id_t> {
+  // skeleton (dim: number): cell_complex_t {
+  //   [todo]
+  // }
+
+  gen_id (dim: number): id_t {
+    let ser: number
+    if (dim === 0) {
+      ser = this.point_array.length
+    } else {
+      ser = 0
+      for (let id of this.cell_map.keys ()) {
+        if (id.dim === dim) {
+          ser += 1
+        }
+      }
+    }
+    return new id_t (dim, ser)
+  }
+
+  add_points (n: number): Array <id_t> {
     let array: Array <id_t> = []
-    let size = this.point_set.size
+    let size = this.point_array.length
     for (let i = size;
          i < size + n;
          i += 1) {
       let id = new id_t (0, i)
-      this.point_set.add (id)
       array.push (id)
     }
+    this.point_array = this.point_array.concat (array)
     return array
   }
+
+  add_cell (cell: cell_t): id_t {
+    let id = this.gen_id (cell.dim)
+    this.cell_map.set (id, cell)
+    return id
+  }
+
+  spherical_p (): boolean {
+    // [todo]
+    return true
+  }
+
+  as_spherical (): spherical_complex_t {
+    let spherical_complex = new spherical_complex_t ()
+    spherical_complex.from_cell_complex (this)
+    return spherical_complex
+  }
+}
+
+export
+class spherical_check_t {
+  constructor () {}
 }
 
 export
 class spherical_complex_t
 extends cell_complex_t {
   info: {
-    spherical_p: true
+    spherical_check: spherical_check_t
   }
 
-  constructor (
-    cell_complex: cell_complex_t,
-  ) {
+  constructor () {
     super ()
-    this.point_set = cell_complex.point_set
+    let spherical_check = new spherical_check_t ()
+    this.info = { spherical_check }
+  }
+
+  // - self-builder
+  from_cell_complex (
+    cell_complex: cell_complex_t
+  ): spherical_check_t {
+    this.point_array = cell_complex.point_array
     this.cell_map = cell_complex.cell_map
 
-    if (spherical_complex_p (cell_complex)) {
-      this.info = { spherical_p: true }
+    if (cell_complex.spherical_p ()) {
     } else {
       throw new Error ("spherical check failed")
     }
+
+    return this
+  }
+}
+
+// - use inheritance to specify more concrete constructions
+//   and extends the interface to meet specific needs
+// - since typescript has no multiple inheritance
+//   we use explicit `.as_*` to mimic this missing feature
+
+export
+class empty_t
+extends cell_complex_t {
+  constructor () {
+    super ()
   }
 }
 
 export
-function spherical_complex_p (
-  cell_complex: cell_complex_t
-): boolean {
-  // [todo]
-  return true
+class discrete_space_t
+extends cell_complex_t {
+  constructor () {
+    super ()
+  }
+
+  from_number (n: number): discrete_space_t {
+    this.add_points (n)
+    return this
+  }
+
+  idx (i: number): id_t {
+    return this.point_array [i]
+  }
 }
 
-// [todo] skeleton
+export
+class singleton_t
+extends discrete_space_t {
+  point: id_t
+
+  constructor () {
+    super ()
+    this.from_number (1)
+    this.point = this.idx (0)
+  }
+}
+
+export
+class start_and_end_t
+extends discrete_space_t {
+  start: id_t
+  end: id_t
+
+  constructor () {
+    super ()
+    this.from_number (2)
+    this.start = this.idx (0)
+    this.end = this.idx (1)
+  }
+}
+
+export
+class interval_t
+extends cell_complex_t {
+  arrow: id_t
+  start: id_t
+  end: id_t
+
+  constructor () {
+    super ()
+    let start_and_end = new start_and_end_t ()
+    let [start, end] = this.add_points (2)
+    let dom = start_and_end.as_spherical ()
+    let cod = this
+    let cell = new cell_t (1, dom, cod, new Map ([
+      [start_and_end.start, start],
+      [start_and_end.end, end]
+    ]))
+    this.arrow = this.add_cell (cell)
+    this.start = start
+    this.end = end
+  }
+}
+
+console.log (new interval_t ())
--- tests/cx.test.js
+++ tests/cx.test.js
@@ -7,22 +7,3 @@ import {
   cell_complex_t,
   spherical_complex_t
 } from "../dist/cx"
-
-test ("empty", t => {
-  let empty = new cell_complex_t ()
-
-  t.true (empty.point_set.size === 0)
-  t.true (empty.cell_map.size === 0)
-})
-
-test ("singleton", t => {
-  let singleton = new cell_complex_t ()
-
-  let id_array = singleton.inc_points (1)
-
-  t.true (id_array.length === 1)
-  t.true (singleton.point_set.size === 1)
-  t.true (singleton.cell_map.size === 0)
-})
-
-test.todo ("torus")
